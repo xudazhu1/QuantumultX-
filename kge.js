35a0f9e07d9b1589a3fa1418894aca9eece12f7e
easy <42052283+xudazhu1@users.noreply.github.com>
2024-03-21 06:25:10 +0800

    Update kge.js

--- kge.js
+++ kge.js
@@ -1,9 +1,7 @@
+console.log($request.body);
 let body = $request.body;
-console.log(body)
-// "cost_time" : 3217,
+
 body = JSON.stringify(body).replace(/"cost_time":"?[\d|.]+"?[,]/g,"\"cost_time\":\"0.00\",");
-// body = body.replace(/&locale=zh_CN/,"&locale=zh_PH");
-// body = body.replace(/&s_locale=zh-Hans_[A-Z]{2}/,"&s_locale=zh-Hans_PH");
 
 
 
