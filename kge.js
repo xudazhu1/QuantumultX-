d6267582b590b55222f0d7b47dc438e2b2000d5e
easy <42052283+xudazhu1@users.noreply.github.com>
2024-03-21 06:08:04 +0800

    Update kge.js

--- kge.js
+++ kge.js
@@ -1,6 +1,6 @@
 let body = $request.body;
 // "cost_time" : 3217,
-body = body.replace(/"cost_time" : \d+/,"\"cost_time\" : \"0.00\"");
+body = body.replace(/"cost_time":"?[\d|.]+"?[,]/g,"\"cost_time\":\"0.00\",");
 // body = body.replace(/&locale=zh_CN/,"&locale=zh_PH");
 // body = body.replace(/&s_locale=zh-Hans_[A-Z]{2}/,"&s_locale=zh-Hans_PH");
 
