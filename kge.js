84d3cb1704adc1b5a7ee08de881d4b845187e1cc
easy <42052283+xudazhu1@users.noreply.github.com>
2024-03-21 06:36:20 +0800

    Update kge.js

--- kge.js
+++ kge.js
@@ -1,8 +1,12 @@
 // console.log($request.body);
 let body = $request.body;
 
-body = JSON.stringify(body).replace(/"cost_time":"?[\d|.]+"?[,]/g,"\"cost_time\":\"0.00\",");
+// body = JSON.stringify(body).replace(/"cost_time":"?[\d|.]+"?[,]/g,"\"cost_time\":\"0.00\",");
+for (let obj of body1.events) {
+    if (obj.ext) {
+        obj.ext.cost_time = "0.00"
+    }
+}
 
 
-
-$done({JSON.parse(body)});
+$done({body});
