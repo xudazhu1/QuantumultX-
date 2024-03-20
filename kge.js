7053c55b10d1f908e0d0e4fd205243f2c3b1390f
easy <42052283+xudazhu1@users.noreply.github.com>
2024-03-21 06:27:14 +0800

    Update kge.js

--- kge.js
+++ kge.js
@@ -1,8 +1,8 @@
-console.log($request.body);
+// console.log($request.body);
 let body = $request.body;
 
 body = JSON.stringify(body).replace(/"cost_time":"?[\d|.]+"?[,]/g,"\"cost_time\":\"0.00\",");
 
 
 
-$done({body});
+$done({JSON.parse(body)});
