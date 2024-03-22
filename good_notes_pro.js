6bd7cd798ed971fd65ae48fdd3f74220ed64b5e7
easy <42052283+xudazhu1@users.noreply.github.com>
2024-03-22 11:54:37 +0800

    Update good_notes_pro.js

--- good_notes_pro.js
+++ good_notes_pro.js
@@ -5,8 +5,8 @@ author：chxm1023
 
 **************************************
 [rewrite_local]
-^https:\/\/isi\..*\.g.*\.(com\..*|com)\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body http://192.168.8.229:8088/good_notes_pro.js
-^https:\/\/isi\..*\.g.*\.(com\..*|com)\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header http://192.168.8.229:8088/good_notes_pro.js
+^https:\/\/isi\..*\.g.*\.(com\..*|com)\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/xudazhu1/QuantumultX-/main/good_notes_pro.js
+^https:\/\/isi\..*\.g.*\.(com\..*|com)\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/xudazhu1/QuantumultX-/main/good_notes_pro.js
 
 [mitm]
 hostname = isi.*.g*.com*
